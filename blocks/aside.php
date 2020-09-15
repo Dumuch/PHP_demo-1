c6ce340ffbe27986a40caebf9270bac50ecee439
Dumuch <dumuch@yandex.ru>
2020-09-15 19:26:11 +0800

    Настройка основных блоков

---
+++ blocks/aside.php
@@ -0,0 +1,14 @@
+<aside class="col-md-4">
+  <div class="p-3 mb-3 bg-warning rounded">
+    <h4><b>Интересные факты</b></h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Officiis, qui quidem cumque. Doloribus eos molestiae eaque
+      tenetur laboriosam ipsa alias doloremque consequatur, et sapiente
+      dicta facilis quisquam modi soluta. Temporibus.
+    </p>
+  </div>
+  <div class="p-3 mb-3">
+    <img class="img-thumbnail"
+      src="https://million-wallpapers.ru/wallpapers/4/28/17578182098611079228/ozero-gory-nebo.jpg" alt="">
+  </div>
+</aside>
--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-werwerew
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP блог</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="icon" href="img/favicon.png">
+</head>
+<body>
+  <?php require 'blocks/header.php';?>
+
+
+  <main class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 mb-3">
+        Основная часть сайта
+      </div>
+
+    <?php require 'blocks/aside.php';?>
+
+    </div>
+  </main>
+
+
+  <?php require 'blocks/footer.php';?>
+
+</body>
+</html>
